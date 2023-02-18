5aec1709c06acd9515e82a5776e2c4f2d48edabf
rossie111 <78100554+rossie111@users.noreply.github.com>
2023-02-17 18:39:15 -0800

    Added JS file and script

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+$('document').ready(function(){
+	alert('hi');
+})
+
+$('document').ready(function(){
+	var mouseIdle;
+	var screensaverState = false;
+	var timeIdle = 3;
+
+	function displayScreensaver(){
+		$('.screensaver').fadeIn();
+		screensaverState = true;
+	};
+
+	function stopScreensaver(){
+		$('.screensaver').fadeOut();
+		screensaverState = false;
+	};
+
+	$('document').mousemove(function(){
+		clearTimeout(mouseIdle);
+
+		if (screensaverState) {
+		stopScreensaver();
+		}
+
+		mouseIdle = setTimeout(function(){
+			displayScreensaver();
+		}, 1000 * timeIdle);
+	});
+
+	$(document).mousemove(function(){
+		clearTimeout(mouseIdle);
+
+		mouseIdle = setTImeout(function(){
+			displayScreensaver();
+		}, 1000 * timeIdle);
+	});
+
+});
